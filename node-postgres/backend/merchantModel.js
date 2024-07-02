02a181500119891e8126c0b7cf366ea42769f62b
Fabio <fabio@axicon.no>
2024-07-02 16:20:00 +0200

    Wip react-postgres

---
+++ node-postgres/backend/merchantModel.js
@@ -0,0 +1,97 @@
+
+//Please note that putting credentials such as user, host, database, password, 
+//and port like in the example above is not recommended in a production environment.
+// We’ll keep it in this file to simplify the tutorial.
+
+const Pool = require("pg").Pool;
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "my_database",
+  password: "ax",
+  port: 5432,
+});
+//get all merchants our database
+const getMerchants = async () => {
+  try {
+    return await new Promise(function (resolve, reject) {
+      pool.query("SELECT * FROM merchants", (error, results) => {
+        if (error) {
+          reject(error);
+        }
+        if (results && results.rows) {
+          resolve(results.rows);
+        } else {
+          reject(new Error("No results found"));
+        }
+      });
+    });
+  } catch (error_1) {
+    console.error(error_1);
+    throw new Error("Internal server error");
+  }
+};
+
+//create a new merchant record in the databsse
+const createMerchant = (body) => {
+  return new Promise(function (resolve, reject) {
+    const { name, email } = body;
+    pool.query(
+      "INSERT INTO merchants (name, email) VALUES ($1, $2) RETURNING *",
+      [name, email],
+      (error, results) => {
+        if (error) {
+          reject(error);
+        }
+        if (results && results.rows) {
+          resolve(
+            `A new merchant has been added: ${JSON.stringify(results.rows[0])}`
+          );
+        } else {
+          reject(new Error("No results found"));
+        }
+      }
+    );
+  });
+};
+//delete a merchant
+const deleteMerchant = (id) => {
+  return new Promise(function (resolve, reject) {
+    pool.query(
+      "DELETE FROM merchants WHERE id = $1",
+      [id],
+      (error, results) => {
+        if (error) {
+          reject(error);
+        }
+        resolve(`Merchant deleted with ID: ${id}`);
+      }
+    );
+  });
+};
+//update a merchant record
+const updateMerchant = (id, body) => {
+  return new Promise(function (resolve, reject) {
+    const { name, email } = body;
+    pool.query(
+      "UPDATE merchants SET name = $1, email = $2 WHERE id = $3 RETURNING *",
+      [name, email, id],
+      (error, results) => {
+        if (error) {
+          reject(error);
+        }
+        if (results && results.rows) {
+          resolve(`Merchant updated: ${JSON.stringify(results.rows[0])}`);
+        } else {
+          reject(new Error("No results found"));
+        }
+      }
+    );
+  });
+};
+module.exports = {
+  getMerchants,
+  createMerchant,
+  deleteMerchant,
+  updateMerchant
+};
---
+++ node-postgres/frontend/src/App.jsx
@@ -0,0 +1,112 @@
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [merchants, setMerchants] = useState(false);
+
+  function getMerchant() {
+    fetch('http://localhost:3001')
+      .then(response => {
+        return response.text();
+      })
+      .then(data => {
+        setMerchants(data);
+      });
+  }
+
+  function createMerchant() {
+    let name = prompt('Enter merchant name');
+    let email = prompt('Enter merchant email');
+    fetch('http://localhost:3001/merchants', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({name, email}),
+    })
+      .then(response => {
+        return response.text();
+      })
+      .then(data => {
+        alert(data);
+        getMerchant();
+      });
+  }
+
+  function deleteMerchant() {
+    let id = prompt('Enter merchant id');
+    fetch(`http://localhost:3001/merchants/${id}`, {
+      method: 'DELETE',
+    })
+      .then(response => {
+        return response.text();
+      })
+      .then(data => {
+        alert(data);
+        getMerchant();
+      });
+  }
+
+  function updateMerchant() {
+    let id = prompt('Enter merchant id');
+    let name = prompt('Enter new merchant name');
+    let email = prompt('Enter new merchant email');
+    fetch(`http://localhost:3001/merchants/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({name, email}),
+    })
+      .then(response => {
+        return response.text();
+      })
+      .then(data => {
+        alert(data);
+        getMerchant();
+      });
+  }
+
+  useEffect(() => {
+    getMerchant();
+  }, []);
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+       <div>
+      {merchants ? merchants : 'There is no merchant data available'}
+      <br />
+      <button onClick={createMerchant}>Add merchant</button>
+      <br />
+      <button onClick={deleteMerchant}>Delete merchant</button>
+      <br />
+      <button onClick={updateMerchant}>Update merchant</button>
+    </div>
+    </>
+  )
+}
+
+export default App
